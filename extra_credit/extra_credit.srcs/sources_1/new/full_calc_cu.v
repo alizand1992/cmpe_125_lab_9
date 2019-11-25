cec8fd1e17a728e258f37402bcbca2869f01eb51
Ali Zand <ali.zand@sjsu.edu>
2019-11-25 15:39:22 -0800

    stuff

---
+++ extra_credit/extra_credit.srcs/sources_1/new/full_calc_cu.v
@@ -0,0 +1,13 @@
+module full_calc_cu(
+    input go,
+    input reg [2:0] f,
+    
+    output en_f, en_x, en_y, go_calc, go_div, op_calc, s1, s2,
+    output set_h, set_l
+);
+
+    always @ (f)
+    begin
+
+
+endmodule
