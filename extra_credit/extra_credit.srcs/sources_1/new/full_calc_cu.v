5e74d5a16ed5177fa3a39816ad004979ec0393f0
Ali Zand <ali.zand@sjsu.edu>
2019-12-02 14:07:05 -0800

    Some fucking shit

--- extra_credit/extra_credit.srcs/sources_1/new/full_calc_cu.v
+++ extra_credit/extra_credit.srcs/sources_1/new/full_calc_cu.v
@@ -1,24 +1,60 @@
 module full_calc_cu(
-    input go, clk,
+    input go, clk, rst,
     input [2:0] f,
     
-    output en_f, en_x, en_y, go_calc, go_div, set_h, set_l,
+    output en_f, en_x, en_y, go_calc, go_div, sel_h,
     output en_out_h, en_out_l,
-    output reg y_sel, x_sel,
-    output reg [1:0] op_calc
+    output [1:0] sel_l,
+    output y_sel, x_sel,
+    output [1:0] op_calc
 );
 
-    always @(f)
+    reg [3:0] cs, ns;
+    reg [14:0] controls;
+    assign { op_calc, sel_l, en_f, en_x, en_y, go_calc, go_div, sel_h, en_out_h, en_out_l, y_sel, x_sel, done } = controls;
+
+    always @ (posedge clk, posedge rst)
     begin
-        case(f)
-            3'b000: { op_calc, y_sel, x_sel } = 4'b00_1_1;
-            3'b001: { op_calc, y_sel, x_sel } = 4'b01_1_1;
-            3'b010: { op_calc, y_sel, x_sel } = 4'b00_1_1;
-            3'b011: { op_calc, y_sel, x_sel } = 4'b00_1_1;
-            3'b100: { op_calc, y_sel, x_sel } = 4'b00_0_1;
-            3'b101: { op_calc, y_sel, x_sel } = 4'b01_0_1;
-            3'b110: { op_calc, y_sel, x_sel } = 4'b00_1_0;
-            default: { op_calc, y_sel, x_sel } = 4'b00_0_0;
-        endcase
+        if (rst) begin
+            cs = 0;
+            ns = 0;
+        end
+        else begin
+            cs = ns;
+            case (cs) 
+                4'b0000: 
+                begin 
+                    controls = 15'b00_00_0_0_0_0_0_0_0_0_0_0_0;
+                    ns = 1;    
+                end
+                4'b0001: 
+                begin
+                    controls = 15'b00_00_1_1_1_0_0_0_0_0_0_0_0;
+                    ns = 2;
+                end
+                4'b0010: 
+                begin
+                    controls = 15'b00_00_0_0_0_0_0_0_0_0_0_0_0;
+                    case (f) 
+                        3'b000: ns = 3; 
+                        3'b001: ns = 4;
+                        3'b010: ns = 5;
+                        3'b011: ns = 6;
+                        3'b100: ns = 7;
+                        3'b101: ns = 8;
+                        3'b110: ns = 9;
+                        3'b111: ns = 0;
+                    endcase;    
+                end
+                4'b0011: controls = 15'b00_10_0_0_0_1_0_0_1_1_1_1_0;
+                4'b0100: controls = 15'b01_10_0_0_0_1_0_0_1_1_1_1_0;
+                4'b0101: controls = 15'b00_01_0_0_0_0_0_1_1_1_0_0_0;
+                4'b0110: controls = 15'b00_00_0_0_0_0_1_0_1_1_0_0_0;
+                4'b0111: controls = 15'b00_10_0_0_0_1_0_0_1_1_0_1_0;
+                4'b1000: controls = 15'b01_10_0_0_0_1_0_0_1_1_0_1_0;
+                4'b1001: controls = 15'b00_00_0_0_0_0_0_0_1_1_1_0_0;
+                4'b1010: controls = 15'b00_00_0_0_0_0_0_0_0_0_0_0_1;
+            endcase
+        end
     end
 endmodule
